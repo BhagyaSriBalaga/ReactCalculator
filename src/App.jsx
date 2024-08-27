6b730023679d731eb40b2dad4b1e4863cc245495
BhagyaSri08 <bbhagyasribalaga@gmail.com>
2024-08-27 16:56:28 +0530

    Hello

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Calci from './Calci'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Calci/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Calci.jsx
@@ -0,0 +1,42 @@
+import React,{useState} from 'react'
+import './Calci.css';
+import Keyboard from '../../src/Keyboard'
+function Calci() {
+    let [input,setInput]=useState("0");
+    function handleClick(value){
+      // value = value.replace(/\b0+(\d)/g, '$1');
+      if(input =="0")
+        {
+          setInput(value);
+          return;
+        }
+        setInput(input+value);
+       
+     }
+    
+
+    function calculate(){
+  
+      let outputVal=eval(input);
+      setInput(outputVal);
+    }
+
+    function handleClear(){
+      setInput("");
+    }
+ 
+  return (
+    <>
+    <div className='container'>
+        <h1>Calci using React</h1>
+        <div className='calculator'>
+            <input type='text' value={input} className='output' onChange={(e)=>setInput(e.target.value)}/>
+            <Keyboard handleClick={handleClick} handleClear={handleClear} calculate={calculate} />
+        </div>
+    </div>
+    </>
+    
+  )
+}
+
+export default Calci
